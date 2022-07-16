bd7652a2a6b39e802dc4a9ddb9e8a89c3e0328d3
ak-hayashi <eight.kayst@gmail.com>
2022-07-17 01:27:31 +0900

    4-6 詳細画面

--- book/urls.py
+++ book/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("book", views.ListBookView.as_view())]
+urlpatterns = [
+    path("book", views.ListBookView.as_view()),
+    path("book/<int:pk>/detail/", views.DetailBookView.as_view()),
+]
--- book/views.py
+++ book/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Book
 
 # Create your views here.
 class ListBookView(ListView):
     template_name = "book/book_list.html"
+    model = Book
+
+
+class DetailBookView(DetailView):
+    template_name = "book/book_detail.html"
     model = Book
