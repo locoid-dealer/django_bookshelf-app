edc6d4dc32d5f07582839ea9d14770ace8f712f6
ak-hayashi <eight.kayst@gmail.com>
2022-07-17 23:56:21 +0900

    4-10 DeleteView

--- book/views.py
+++ book/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import Book
 
 # Create your views here.
@@ -18,4 +18,10 @@ class CreateBookView(CreateView):
     template_name = "book/book_create.html"
     model = Book
     fields = {"title", "text", "category"}
+    success_url = reverse_lazy("list-book")
+
+
+class DeleteBookView(DeleteView):
+    template_name = "book/book_confirm_delete.html"
+    model = Book
     success_url = reverse_lazy("list-book")
