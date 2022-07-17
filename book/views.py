98af041dc46347acac11c87ace07ff2fd70adb20
ak-hayashi <eight.kayst@gmail.com>
2022-07-18 00:01:10 +0900

    4-11 UpdateView

--- book/views.py
+++ book/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView,
+    UpdateView,
+)
 from .models import Book
 
 # Create your views here.
@@ -24,4 +30,11 @@ class CreateBookView(CreateView):
 class DeleteBookView(DeleteView):
     template_name = "book/book_confirm_delete.html"
     model = Book
+    success_url = reverse_lazy("list-book")
+
+
+class UpdateBookView(UpdateView):
+    template_name = "book/book_update.html"
+    model = Book
+    fields = {"title", "text", "category"}
     success_url = reverse_lazy("list-book")
